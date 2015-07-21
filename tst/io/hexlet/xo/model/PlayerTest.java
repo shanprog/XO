0fde1b82cd9466b208f56536a79eb3e5e98f4ff3
ShAN <shan.prog@gmail.com>
2015-07-21 16:17:00 +0900

    PlayerTest - created

---
+++ tst/io/hexlet/xo/model/PlayerTest.java
@@ -0,0 +1,33 @@
+package io.hexlet.xo.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void testGetName() throws Exception {
+
+        final String inputValue = "Slava";
+        final String expectedValue = inputValue;
+
+        final Player player = new Player(inputValue, null);
+
+        final String actualValue = player.getName();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void testGetFigure() throws Exception {
+        final Figure inputValue = Figure.X ;
+        final Figure expectedValue = inputValue;
+
+        final Player player = new Player(null, inputValue);
+
+        final Figure actualValue = player.getFigure();
+
+        assertEquals(expectedValue, actualValue);
+    }
+}
